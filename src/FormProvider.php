e1099addd62a627c5700a40d6c5c178f8044b13b
Patrick Leblanc <patrick@flagshipcompany.com>
2015-04-02 11:15:58 -0400

    Adds a form helper

---
+++ src/FormProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flagship\Components\Helpers;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class FormProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['flagship.helpers.forms.getErrorMessages'] = $app->protect(function (\Symfony\Component\Form\Form $form) {
+            $errors = array();
+
+            foreach ($form->getErrors() as $key => $error) {
+                if ($form->isRoot()) {
+                    $errors['#'][] = $error->getMessage();
+                } else {
+                    $errors[] = $error->getMessage();
+                }
+            }
+
+            foreach ($form->all() as $child) {
+                if (!$child->isValid()) {
+                    $errors[$child->getName()] = $this->getErrorMessages($child);
+                }
+            }
+
+            return $errors;
+        });
+    }
+}
